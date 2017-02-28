c55efb87eb94d78caa8be1015aed7740e02625ba
Evoliofly <antoine.murat@protonmail.com>
2017-02-28 11:59:05 +0100

    Antoine 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
+function updateURL(hash){
+	
+	hash = hash.replace( /^#/, '' )
+	var node = $( '#' + hash )
+	node.attr( 'id', '' )
+	document.location.hash = hash
+	node.attr( 'id', hash )
+
+	setTimeout(function(){
+		$(window).scroll()
+	}, 25)
+}
+
 $(function(){
 	//$(".button-collapse").sideNav();
 
-	$(".card-action a, .brand-logo, ul li a").click(function() {
+	$("[data-dest]").click(function() {
+		window.location.hash = $(this).data('dest')
 		$('ul.tabs').tabs('select_tab', $(this).data('dest'))
-		setTimeout(function(){
-			$(window).scroll()
-		}, 25);
+		updateURL($(this).data('dest'))
+	})
+
+	$('ul.tabs').on('click', 'a', function(e) {
+		updateURL($(this).attr('href'))
 	})
 
 	$('.parallax').parallax()
